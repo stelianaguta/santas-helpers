7fe7bcb4a48c92229302d44aa841c185ade0ce2c
Steliana Guta <steliana.guta@gmail.com>
2012-11-25 20:41:45 +0100

    added placeholders in the settings

--- settings.php
+++ settings.php
@@ -1,7 +1,7 @@
 <?php
-	 $DB_HOSTNAME = "127.0.0.1";
-	 $DB_NAME = 'helpers';
-	 $DB_USERNAME = 'root';
-	 $DB_PASS = '';
+	 $DB_HOSTNAME = "<<put_your_hostname_here>>";
+	 $DB_NAME = '<<put_your_db_name_here>>';
+	 $DB_USERNAME = '<<put_the_username_here>>';
+	 $DB_PASS = '<<put_the_password_here>>';
 	 $DEFAULT_LANGUAGE = "en";
 ?>
